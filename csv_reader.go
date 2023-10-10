f5ef16f760c775966dcde0685d6ba7a2743b0a91
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-10 20:11:19 +0300

    added extension filter

--- csv_reader.go
+++ csv_reader.go
@@ -2,24 +2,43 @@ package mongoimport
 
 import (
 	"encoding/csv"
+	"errors"
 	"log"
 	"os"
+	"strings"
 )
 
 // CSVReader func for csv file reader
 func CSVReader(filename string) ([][]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Printf("Error opening csv file %v", err)
-		return nil, err
+	substring := strings.Split(filename, ".")
+	var extension string
+	if len(substring) > 1 {
+		extension = substring[len(substring)-1]
+	} else {
+		return nil, errors.New("invalid file passed")
 	}
-	defer file.Close()
+	if strings.Compare("csv", extension) == 0 {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Printf("Error opening csv file %v", err)
+			return nil, err
+		}
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				log.Printf("%s", err)
+				return
+			}
+		}(file)
 
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
-	if err != nil {
-		log.Printf("Error reading csv file %v", err)
-		return nil, err
+		reader := csv.NewReader(file)
+		records, err := reader.ReadAll()
+		if err != nil {
+			log.Printf("Error reading csv file %v", err)
+			return nil, err
+		}
+		return records, nil
+	} else {
+		return nil, errors.New("csv file required")
 	}
-	return records, nil
 }
