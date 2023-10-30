ef83ccc461589a3417031b2a9a66ac90921e7c2f
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-30 15:31:25 +0300

    json import [switch from mgo.v2 -> mongo-driver1.12.1]

--- json/json_import.go
+++ json/json_import.go
@@ -1,26 +1,48 @@
 package json
 
 import (
-	"log"
-
-	"gopkg.in/mgo.v2"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // JSONImport function import/write json contents into a mongodb collection
-func JSONImport(collection *mgo.Collection, contents map[string]interface{}) (int, error) {
-	bulk := collection.Bulk()
-	bulk.Unordered()
+//func JSONImport(collection *mgo.Collection, contents map[string]interface{}) (int, error) {
+//	bulk := collection.Bulk()
+//	bulk.Unordered()
+//
+//	bulk.Insert(contents)
+//	_, err := bulk.Run()
+//	if err != nil {
+//		log.Fatal(err)
+//		return 0, err
+//	}
+//	count, err := collection.Count()
+//	if err != nil {
+//		log.Fatal(err)
+//		return 0, err
+//	}
+//	return count, nil
+//}
 
-	bulk.Insert(contents)
-	_, err := bulk.Run()
+func JSONImport(collection *mongo.Collection, contents map[string]interface{}) (int, error) {
+	var bulkModels []mongo.WriteModel
+
+	for i := 0; i < len(contents); i++ {
+		bulkModels = append(bulkModels, mongo.NewInsertOneModel().SetDocument(contents))
+	}
+
+	ctx := context.TODO()
+	_, err := collection.BulkWrite(ctx, bulkModels)
+
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 		return 0, err
 	}
-	count, err := collection.Count()
+	count, err := collection.CountDocuments(ctx, bson.D{})
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 		return 0, err
 	}
-	return count, nil
+	return int(count), nil
 }
