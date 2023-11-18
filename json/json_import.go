76211575257bc06e7826816a8675ab37826458f4
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-11-18 07:35:21 +0300

    removed stale code

--- json/json_import.go
+++ json/json_import.go
@@ -6,25 +6,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// JSONImport function import/write json contents into a mongodb collection
-//func JSONImport(collection *mgo.Collection, contents map[string]interface{}) (int, error) {
-//	bulk := collection.Bulk()
-//	bulk.Unordered()
-//
-//	bulk.Insert(contents)
-//	_, err := bulk.Run()
-//	if err != nil {
-//		log.Fatal(err)
-//		return 0, err
-//	}
-//	count, err := collection.Count()
-//	if err != nil {
-//		log.Fatal(err)
-//		return 0, err
-//	}
-//	return count, nil
-//}
-
 func JSONImport(collection *mongo.Collection, contents map[string]interface{}) (int, error) {
 	var bulkModels []mongo.WriteModel
 
